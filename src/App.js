402acd01d5296d86a4539ac75d879f162ff65823
Naz Malik <nazmalik08@gmail.com>
2022-05-28 12:15:37 +0100

    responsive

--- src/App.js
+++ src/App.js
@@ -25,11 +25,11 @@ function App() {
         <Container px={6} py={4} my={12} maxW="container.md">
             <Flex justify="space-between" align="center">
                 <HStack align="center" spacing={6}>
-                    <Image src='nws-logo.png' alt='whale' h={150} bg={"blue.200"} p={2} rounded="100%"/>
-                    <Heading mt={4}>National Whale Services</Heading>
+                    <Image src='nws-logo.png' alt='whale' h={[65, 150]} bg={"blue.200"} p={2} rounded="100%"/>
+                    <Heading mt={4} size={["md", "xl"]}>National Whale Services</Heading>
                 </HStack>
                 <Image cursor="pointer" src={playing ? "speaker3.png" : "speaker.png"}
-                       alt='whale' h={70}
+                       alt='whale' h={[42, 70]}
                        onClick={onSpeakerClick}/>
             </Flex>
 
